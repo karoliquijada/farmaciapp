a7f5532cd40521c7010c793c99d3dad578f8bcf7
Daniel Guarimata <danigt01360@gmail.com>
2023-07-25 16:33:50 -0430

    Changes Apply BDD (Cardinal rule)

--- compras/models.py
+++ compras/models.py
@@ -4,20 +4,61 @@ from django.db import models
 
 # Create your models here.
 
+class Nombre_Supervisor(models.Model):
+    cedula = models.CharField(max_length=500, primary_key=True, unique=True)
+    primer_nombre = models.CharField(max_length=200)
+    segundo_nombre = models.CharField(max_length=200, null=True, blank=True)
+    primer_apellido = models.CharField(max_length=200)
+    segundo_apellido = models.CharField(max_length=200, null=True, blank=True)
+ 
+    def __str__(self):
+        return (self.primer_nombre, self.primer_apellido)
+    
+    class Meta:
+        db_table = 'Nombre_Supervisor'
+        verbose_name = 'Nombre_Supervisor'
+        verbose_name_plural = 'Nombres_Supervisores'
+        ordering = ['cedula']
+
 class Compra(models.Model):
     id = models.AutoField(primary_key=True, unique=True)
-    producto = models.ForeignKey(Producto, on_delete = models.CASCADE, verbose_name='Producto')
-    proveedor = models.ForeignKey(Proveedor, on_delete = models.CASCADE, verbose_name='Proveedor')
     fecha_de_compra = models.DateField(auto_now_add=True, verbose_name='Fecha')
-    supervisor = models.CharField(max_length=500, verbose_name='Supervisor')
+    supervisor_cedula = models.ForeignKey(Nombre_Supervisor, on_delete = models.CASCADE, verbose_name='Supervisor_Cedula')
     inversion = models.DecimalField(default=0.00, max_digits=20, decimal_places=2)
 
     def __str__(self):
         return self.id
     
     class Meta:
-
         db_table = 'Compra'
-        verbose_name = 'Compras'
-        verbose_name_plural = 'Compra'
+        verbose_name = 'Compra'
+        verbose_name_plural = 'Compras'
+        ordering = ['id']
+
+class MN_Compra_Producto(models.Model):#COMPRA-PRODUCTO (M:N)
+    producto = models.ForeignKey(Producto, on_delete = models.CASCADE)
+    compra = models.ForeignKey(Compra, on_delete = models.CASCADE)
+    id = models.AutoField(primary_key=True, unique=True)
+
+    def __str__(self):
+        return self.id
+    
+    class Meta:
+        db_table = 'MN_Compra_Producto'
+        verbose_name = 'MN_Compra_Producto'
+        verbose_name_plural = 'MN_Compras_Productos'
+        ordering = ['id']
+
+class MN_Compra_Proveedor(models.Model):#COMPRA-PROVEEDOR (M:N)
+    proveedor = models.ForeignKey(Proveedor, on_delete = models.CASCADE, verbose_name='Proveedor')
+    compra = models.ForeignKey(Compra, on_delete = models.CASCADE)
+    id = models.AutoField(primary_key=True, unique=True)
+
+    def __str__(self):
+        return self.id
+    
+    class Meta:
+        db_table = 'MN_Compra_Proveedor'
+        verbose_name = 'MN_Compra_Proveedor'
+        verbose_name_plural = 'MN_Compra_Proveedor'
         ordering = ['id']
--- productos/models.py
+++ productos/models.py
@@ -2,12 +2,26 @@ from django.db import models
 
 # Create your models here.
 
+class Categoria_Producto(models.Model):
+    categoria = models.CharField(max_length = 500, primary_key=True, unique=True)
+
+    def __str__(self):
+        return self.categoria
+    
+    class Meta:
+        db_table = 'Categoria_Producto'
+        verbose_name = 'Categoria_Producto'
+        verbose_name_plural = 'Categorias_Productos'
+        ordering = ['categoria']
+
 class Producto(models.Model):
     codigo = models.CharField(primary_key=True, max_length=5, unique=True, verbose_name='Codigo')
     nombre = models.CharField(max_length=150, verbose_name='Nombre')
     fecha_de_vencimiento = models.DateField(auto_now_add=True, verbose_name='Fecha')
     precio = models.DecimalField(default=0.00, max_digits=9, decimal_places=2)
     cantidad = models.PositiveIntegerField(default=0)
+    descuento = models.DecimalField(default=0.00, max_digits=9, decimal_places=2)
+    exento = models.BooleanField()
     imagen_de_producto = models.ImageField(upload_to='img_product', null=True, blank=True, verbose_name='Imagen')  
 
     def __str__(self):
@@ -18,3 +32,17 @@ class Producto(models.Model):
         verbose_name = 'Producto'
         verbose_name_plural = 'Productos'
         ordering = ['codigo']
+
+class MN_Subcategoria_Producto(models.Model):#Categoria - Producto (MN)
+    categoria = models.ForeignKey(Categoria_Producto, on_delete=models.CASCADE)
+    codigo_producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
+    subcategoria = models.CharField(max_length=500, primary_key=True, unique=True)
+
+    def __str__(self):
+        return self.subcategoria
+    
+    class Meta:
+        db_table = 'Subcategoria_Producto'
+        verbose_name = 'Subcategoria_Producto'
+        verbose_name_plural = 'Subcategorias_Productos'
+        ordering = ['subcategoria']
